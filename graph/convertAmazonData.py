bdcd05b58c303564d563be26084a896a5cb9418b
Sequoyha Pelletier <sequoyha@datastax.com>
2016-06-10 09:05:12 -0700

    removed convertMetadata/convertComplete in favor of convertAmazonData.py

--- graph/convertAmazonData.py
+++ graph/convertAmazonData.py
@@ -6,6 +6,10 @@ def parse(path):
   for l in g:
     yield json.dumps(eval(l))
 
+f = open("amazon_metadata.json", 'w')
+for l in parse("metadata.json.gz"):
+  f.write(l + '\n')
+
 f = open("amazon_reviews.json", 'w')
 for l in parse("complete.json.gz"):
   f.write(l + '\n')
